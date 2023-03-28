dd357143967c4655dedb00614957885a1956e41f
SadRus <rystixx@yandex.ru>
2023-03-28 16:45:25 +0300

    Add sending log to a telegram

--- main.py
+++ main.py
@@ -8,6 +8,35 @@ from dotenv import load_dotenv
 from time import time
 
 
+class TelegramLogsHandler(logging.Handler):
+
+    def __init__(self, tg_bot, chat_id):
+        super().__init__()
+        self.chat_id = chat_id
+        self.tg_bot = tg_bot
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        self.tg_bot.send_message(chat_id=self.chat_id, text=log_entry)
+
+
+def create_logger(tg_bot, chat_id):
+    logging.basicConfig(
+        level=logging.DEBUG,
+        filename='bot.log',
+        filemode='w',
+        format="%(asctime)s %(process)s %(levelname)s %(message)s",
+        )
+    logger = logging.getLogger('name_logger')
+    logger.setLevel(logging.INFO)
+    handler = TelegramLogsHandler(
+        tg_bot=tg_bot,
+        chat_id=chat_id,
+    )
+    logger.addHandler(handler)
+    return logger
+
+
 def create_parser():
     parser = argparse.ArgumentParser(
         description='Telegram bot sending tasks notifications',
@@ -24,9 +53,6 @@ def create_parser():
 
 
 def main():
-    logging.basicConfig(level=logging.DEBUG)
-    logging.info('bot started')
-
     load_dotenv()
     parser = create_parser()
     args = parser.parse_args()
@@ -34,14 +60,15 @@ def main():
     dvmn_token = os.getenv('DVMN_TOKEN')
     tg_bot_token = os.getenv('TG_BOT_TOKEN')
     tg_chat_id = args.chat_id
+    tg_bot = telegram.Bot(token=tg_bot_token)
 
-    bot = telegram.Bot(token=tg_bot_token)
+    logger = create_logger(tg_bot, tg_chat_id)
+    logger.info('Bot started')
 
     url = 'https://dvmn.org/api/long_polling/'
     headers = {
         'Authorization': f'Token {dvmn_token}'
     }
-
     timestamp = time()
     while True:
         payload = {'timestamp': timestamp}
@@ -68,9 +95,8 @@ def main():
             lesson_title = last_attempt[-1]['lesson_title']
             lesson_url = last_attempt[-1]['lesson_url']
             attempt_is_negative = last_attempt[-1]['is_negative']
-
             if attempt_is_negative:
-                bot.send_message(
+                tg_bot.send_message(
                     chat_id=tg_chat_id,
                     text=(f'У вас проверили работу "{lesson_title}".\n'
                           f'{lesson_url}\n\n'
@@ -78,7 +104,7 @@ def main():
                           'Исправляйте и возвращайтесь.')
                 )
             else:
-                bot.send_message(
+                tg_bot.send_message(
                     chat_id=tg_chat_id,
                     text=(f'У вас проверили работу "{lesson_title}".\n'
                           f'{lesson_url}\n\nПреподавателю всё понравилось,'
