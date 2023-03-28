a2d906e2847e99949d8fd5a124b3fbb890a9611c
SadRus <rystixx@yandex.ru>
2023-03-29 00:35:55 +0300

    Send logs to Telegram to admin user

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import requests
 import telegram
 
 from dotenv import load_dotenv
-from time import time
 from logging.handlers import RotatingFileHandler
+from time import time
 
 
 class TelegramLogsHandler(RotatingFileHandler):
@@ -24,12 +24,12 @@ class TelegramLogsHandler(RotatingFileHandler):
 
 def create_logger(tg_bot, chat_id):
     logging.basicConfig(
-        level=logging.DEBUG,
+        level=logging.INFO,
         filename='/var/log/bot.log',
         filemode='w',
         format="%(asctime)s %(process)s %(levelname)s %(message)s",
         )
-    logger = logging.getLogger('name_logger')
+    logger = logging.getLogger('tg_bot_logger')
     logger.setLevel(logging.INFO)
     handler = TelegramLogsHandler(
         '/var/log/bot.log',
@@ -74,50 +74,57 @@ def main():
     headers = {
         'Authorization': f'Token {dvmn_token}'
     }
-    timestamp = time()
-    while True:
-        payload = {'timestamp': timestamp}
-        try:
-            response = requests.get(
-                url,
-                headers=headers,
-                params=payload,
-            )
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            logging.exception(e)
-            continue
-        except requests.exceptions.ReadTimeout:
-            continue
-        except requests.exceptions.ConnectionError as e:
-            logging.exception(e)
-            continue
-
-        attempt_description = response.json()
-
-        if attempt_description['status'] == 'timeout':
-            timestamp = attempt_description['timestamp_to_request']
-        else:
-            timestamp = attempt_description['last_attempt_timestamp']
-            last_attempt = attempt_description['new_attempts']
-            lesson_title = last_attempt[-1]['lesson_title']
-            lesson_url = last_attempt[-1]['lesson_url']
-            attempt_is_negative = last_attempt[-1]['is_negative']
-            if attempt_is_negative:
-                tg_bot.send_message(
-                    chat_id=tg_chat_id,
-                    text=(f'У вас проверили работу "{lesson_title}".\n'
-                          f'{lesson_url}\n\n'
-                          'К сожалению в работе есть ошибки. '
-                          'Исправляйте и возвращайтесь.')
+    try:
+        timestamp = time()
+        while True:
+            payload = {'timestamp': timestamp}
+            try:
+                response = requests.get(
+                    url,
+                    headers=headers,
+                    params=payload,
                 )
+                response.raise_for_status()
+            except requests.exceptions.HTTPError as e:
+                logger.exception(e)
+                continue
+            except requests.exceptions.ReadTimeout:
+                continue
+            except requests.exceptions.ConnectionError as e:
+                logger.exception(e)
+                continue
+
+            attempt_description = response.json()
+
+            if attempt_description['status'] == 'timeout':
+                timestamp = attempt_description['timestamp_to_request']
             else:
-                tg_bot.send_message(
-                    chat_id=tg_chat_id,
-                    text=(f'У вас проверили работу "{lesson_title}".\n'
-                          f'{lesson_url}\n\nПреподавателю всё понравилось,'
-                          'можно приступать к следующему уроку')
-                )
+                timestamp = attempt_description['last_attempt_timestamp']
+                last_attempt = attempt_description['new_attempts']
+                lesson_title = last_attempt[-1]['lesson_title']
+                lesson_url = last_attempt[-1]['lesson_url']
+                attempt_is_negative = last_attempt[-1]['is_negative']
+                if attempt_is_negative:
+                    tg_bot.send_message(
+                        chat_id=tg_chat_id,
+                        text=(
+                            f'У вас проверили работу "{lesson_title}".\n'
+                            f'{lesson_url}\n\n'
+                            'К сожалению в работе есть ошибки. '
+                            'Исправляйте и возвращайтесь.'
+                        ),
+                    )
+                else:
+                    tg_bot.send_message(
+                        chat_id=tg_chat_id,
+                        text=(
+                            f'У вас проверили работу "{lesson_title}".\n'
+                            f'{lesson_url}\n\nПреподавателю всё понравилось,'
+                            'можно приступать к следующему уроку'
+                        ),
+                    )
+    except Exception as e:
+        logger.exception(e)
 
 
 if __name__ == '__main__':
